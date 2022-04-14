ce65358c87408a8501fffd9f87ac398d27bb02f6
YounessHassoune <yhassoune7@gmail.com>
2022-04-14 12:15:45 +0000

    dashboard ...

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Dashboard } from "./pages/Dashoard";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Quiz } from "./pages/Quiz";
@@ -10,6 +11,7 @@ export const App=()=>{
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/quiz" element={<Quiz />} />
+                <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/login" element={<Login />} />
              </Routes> 
         </Router>       
