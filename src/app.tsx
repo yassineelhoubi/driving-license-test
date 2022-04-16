3c89f8b1e5cd52271034fad554fd4805396395c0
yassineelhoubi <elhoubiyassine@gmail.com>
2022-04-16 16:47:26 +0100

    create a custom hook to questions

--- src/app.tsx
+++ src/app.tsx
@@ -3,9 +3,16 @@ import { Dashboard } from "./pages/Dashoard";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Quiz } from "./pages/Quiz";
+import UseFetch from "./hook/useFetch";
+import { useEffect } from "react";
 
+export const App = () => {
 
-export const App=()=>{
+    const { data } = UseFetch("https://driving-licence-app.herokuapp.com/question/all")
+    useEffect(() => {
+        console.log(data)
+    }, [data])
+    
     return (
         <Router>
             <Routes>
@@ -13,7 +20,7 @@ export const App=()=>{
                 <Route path="/quiz" element={<Quiz />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/login" element={<Login />} />
-             </Routes> 
-        </Router>       
+            </Routes>
+        </Router>
     )
 }
